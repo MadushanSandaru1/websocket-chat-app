2ebbf866b225cf67c5c244b97d52521f64535191
Madushan Sandaruwan <madushansandaru1@gmail.com>
2022-07-01 11:54:05 +0530

    Bootstrap

---
+++ src/test/java/com/example/websockettest/WebsocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
